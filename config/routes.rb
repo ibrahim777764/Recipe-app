d7a6a603fc316614b2cf585aa762189217eb68a6
ibrahim777764 <ibrahimnaseer896@gmail.com>
2022-10-18 14:55:09 +0500

    create registration form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# frozen_string_literal: true
 
+Rails.application.routes.draw do
+  devise_for :admins
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :admins
   # Defines the root path route ("/")
   # root "articles#index"
 end
